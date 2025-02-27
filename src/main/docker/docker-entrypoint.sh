c1c9df61095c99304fd4b3ea9a9881f0c8a5abe5
Madray Haven <sgpublic2002@gmail.com>
2025-02-27 21:41:55 +0800

    fix: setup scripts

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -4,20 +4,8 @@ set -e
 
 . /setup-functions.sh
 
-run_setup() {
-  local SETUP_D=$1
-  if [[ -d "$SETUP_D" ]]; then
-    for i in $SETUP_D/*.sh; do
-      if [ -r $i ]; then
-        . $i
-      fi
-    done
-    unset i
-  fi
-}
-
-cp -a /etc/app-setup.d/*.sh /etc/app-setup.builtin.d
-run_setup /etc/app-setup.builtin.d
+find /etc/app-setup.d -maxdepth 1 -type f -name "*.sh" -exec cp {} /etc/app-setup.builtin.d \;
+for f in /etc/app-setup.d/*.sh; do [ -f "$f" ] && . "$f"; done
 
 if [[ -z "$APP_CACHE_HOME" ]]; then
   export APP_CACHE_HOME=$XDG_CACHE_HOME/$AUTO_VENV_NAME
